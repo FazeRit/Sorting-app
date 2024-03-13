c20be4ac31a586e2012b15b3d697b6066a80eda2
denis <denisgutzan@gmail.com>
2024-03-13 10:29:37 +0200

    1

---
+++ routes/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        minHeight:400,
+        minWidth:780,
+        autoHideMenuBar: true
+    });
+    mainWindow.webContents.openDevTools();
+    mainWindow.loadFile('./views/mainWindow.html');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
